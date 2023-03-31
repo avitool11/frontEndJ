847a47e614fc62ae43f67d3263292b50e3683bd3
avitool11 <avinash.18cs022@cambridge.edu.in>
2023-04-01 00:59:59 +0530

    New filter added

--- src/App.js
+++ src/App.js
@@ -7,29 +7,33 @@ class App extends Component {
   render() {
    
     return (
+      
       <div className="background-body">
         <div className = "filter-layer">
           <div className='gradient'>
+            <div className='structure1'>          
             <div className='structure'>
               <div className="menu">
                 <div className="n-l">
                   <div className="logo"></div>
                   <div className="name-title"><h1>Tripadvisor</h1></div>
                 </div>
-                <div className='home'>Home</div>
-                <div className='about'>About Us</div>
-                <div className='contact'>Contact Us</div>
+                <div className='home'>HOME</div>
+                <div className='about'>ABOUT US</div>
+                <div className='contact'>CONTACT US</div>
               </div>
               <Link to="/signup"><div className="loginsignupcard">
                 <div className="profilelogo"><CgProfile/></div>
                 <div className="loginsignup-text">Log In or Sign up</div>
               </div></Link>
             </div>
+            
             <div className="content-main">
               <div className="white-line"></div>
               <div className="content-main-text">
                 <p>PLAN YOUR <br/> PERFECT TRIP</p>
               </div>
+            </div>
             </div>
           </div>
         </div>
